75e563b1409392b3c10ed5f94e8d6f345ff5c620
Nathan Hare <nhare330@gmail.com>
2018-02-16 07:21:56 -0500

    Update models

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Page,Tag,Image
+from django.utils.html import format_html
+from datetime import datetime
+import hashlib
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'get_url', 'banner', 'get_banner', 'user', 'published_date')
+    prepopulated_fields = {'slug': ('title',)}
+    def get_banner(self, obj):
+        return format_html(
+                '<a href="{}" target="banner">{}</a>',
+                str(obj.banner_url()),
+                str(obj.banner_url())
+        )
+    get_banner.short_description = 'Banner Path'
+    get_banner.admin_order_field = "banner"
+
+    def get_url(self, obj):
+        return format_html(
+                '<a href="{}" target="post">{}</a>',
+                '/' + str(obj.published_date.year) + '/' + 
+                    str(obj.published_date.month) + '/' + 
+                    str(obj.published_date.day) + '/' + 
+                    obj.slug + '?code=' + obj.code(),
+                'View Post'
+        )
+    get_url.short_description = 'URL'
+    get_url.admin_order_field = "published_date"
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'file')
 
 # Register your models here.
+admin.site.register(Page, PageAdmin)
+admin.site.register(Tag)
+admin.site.register(Image, ImageAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,52 +1,88 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from autoslug import AutoSlugField
-from .tools import recursive_parents
+#from .tools import recursive_parents
+
+class Tag(models.Model):
+        name = models.CharField(max_length=50)
+
+        def __str__(self):
+            return self.name
+
+        class Meta:
+            ordering = ['name']
+
+class Image(models.Model):
+        name = models.CharField(max_length=50)
+        file = models.ImageField()
+        credit = models.TextField(max_length=300, blank=True, null=True)
+
+        def filename(self):
+            return os.path.basename(self.file.name)
+
+        def file_path(self):
+            return '/media/' + str(self.filename())
+
+        def html_credit(self):
+            if self.credit is not None:
+                return markdown.markdown(self.credit)
+            return ''
+
+        def __str__(self):
+            return self.name
+
+        def save(self, *args, **kwargs):
+            super(Image, self).save(*args, **kwargs)
+            filename = self.file.url
+
+        class Meta:
+            ordering = ['name']
 
 class Page(models.Model):
-	TEMPLATE_CHOICES = [
-		('page', 'Page'),
-		('blog', 'Blog'),
-		('post', 'Post'),
-		('story', 'Story'),
-		('chapter', 'Chapter'),
-	]
-	title = models.CharField(max_length=200)
-	slug = AutoSlugField(null=True, populate_from='title')
-	parent = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True)
-	template = models.CharField(max_length=100, choices=TEMPLATE_CHOICES)
-	banner = models.CharField(max_length=1000, blank=True)
-	tags = models.CharField(max_length=1000, blank=True)
-	sidebar = models.TextField(blank=True)
-	body = models.TextField()
-	pub_date = models.DateTimeField(auto_now_add=True)
-	author = models.ForeignKey(User, null=True, blank=True)
+        TEMPLATE_CHOICES = [
+                ('page', 'Page'),
+                ('blog', 'Blog'),
+                ('post', 'Post'),
+                ('story', 'Story'),
+                ('chapter', 'Chapter'),
+        ]
+        title = models.CharField(max_length=200)
+        slug = models.SlugField(max_length=200, null=True)
+        parent = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True)
+        template = models.CharField(max_length=100, choices=TEMPLATE_CHOICES)
+        banner = models.ForeignKey(Image, blank=True, on_delete=models.SET_NULL)
+        body = models.TextField(max_length=10000)
+        tags = models.ManyToManyField(Tag, blank=True)
+        summary = models.TextField(max_length=300, blank=True)
+        sidebar = models.TextField(max_length=1000, blank=True)
+        author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+        pub_date = models.DateTimeField(auto_now_add=True)
+        edit_date = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return self.title
+        def __str__(self):
+                return self.title
 
-	def save_model(self, request, obj, form, change):
-		"""When creating a new object, set the creator field.
-		"""
-		if not change:
-			obj.author = request.user
-		obj.save()
+        def save_model(self, request, obj, form, change):
+                """When creating a new object, set the creator field.
+                """
+                if not change:
+                        obj.author = request.user
+                obj.save()
 
-	def children(self):
-		return Page.objects.filter(parent=self, pub_date__lte=timezone.now())
+        def children(self):
+                return Page.objects.filter(parent=self, pub_date__lte=timezone.now())
 
-	def all_parents(self):
-		if self.parent:
-			return recursive_parents(self.parent)
-		else:
-			return False
+        def all_parents(self):
+                if self.parent:
+                        return recursive_parents(self.parent)
+                else:
+                        return False
 
-	def show_story_title(self):
-		display_in = ['chapter', 'post']
-		if self.parent:
-			if self.template == 'story':
-				return "<h1>" + self.title + "</h1>"
-			elif self.template in display_in:
-				return "<h1>" + self.parent.title + "</h1>"
-		return ''
+        def show_story_title(self):
+                display_in = ['chapter', 'post']
+                if self.parent:
+                        if self.template == 'story':
+                                return "<h1>" + self.title + "</h1>"
+                        elif self.template in display_in:
+                                return "<h1>" + self.parent.title + "</h1>"
+                return ''
